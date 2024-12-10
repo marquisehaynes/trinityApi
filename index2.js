6f8683bba5898f5463a6b528877b66499b0b10e4
Marquise Haynes <haynes.marquise@gmail.com>
2024-12-10 08:30:34 -0600

    restarting process

---
+++ index2.js
@@ -0,0 +1,29 @@
+import * as models from './models/index.js';
+import https	     from 'https';
+import express	   from 'express';
+import fs		   		 from 'fs';
+import pg		   		 from 'pg';
+import Parser	   	 from 'json2csv';
+
+const CopyStream = import( 'pg-copy-streams' );
+const app				 = new express();
+const dbConfig	 = JSON.parse( fs.readFileSync( 'config.json','utf8' ) ).database;
+const pgPool = new pg.Pool({
+	user										: dbConfig.username,
+	password								: dbConfig.password,
+	host										: dbConfig.host,
+	port										: dbConfig.port,
+	database								: dbConfig.dbName,
+	max											: 25,
+	idleTimeoutMillis				: 20000,
+	connectionTimeoutMillis : 20000,
+});
+
+app.listen( 3000, () => {
+    console.log( "Server running on port 3000" );
+   });
+
+app.get( '/syncall', ( req, res ) => {
+    
+});
+
--- util/index.js
+++ util/index.js
@@ -2,16 +2,64 @@ import fs from 'fs';
 export function getCanvasRequestDefinition(targetObj,params){
     const canvasToken= JSON.parse(fs.readFileSync('config.json','utf8')).canvas.authToken;
     const objPathMap = new Map([
-        ["course", "/api/v1/courses/" + params.get('courseId') + "/enrollments?per_page=1000"]
+        ["students", "/api/v1/courses/" + params.get('courseId') + "/enrollments?per_page=1000"],
+        ["courses", '/api/v1/courses?per_page=1000'],
+        ["assignmentgroups", '/api/v1/courses/' + params.get('courseId') + '/assignment_groups?per_page=1000'],
+        ['assignments',''],
+        ['submissions','']
     ]);
     return canvasRequestDef = {
         protocol: 'https:',
         hostname: 'canvas.instructure.com',
         port: 443,
-        path: '/api/v1/courses/' + courseId + '/enrollments?per_page=1000',
+        path: objPathMap.get(targetObj),
         method: 'GET',
         headers: {
             'Authorization': 'Bearer ' + canvasToken
         }
     };
+}
+
+
+export function writeFilePromise(fileName, data, encodingType) {
+	return new Promise((resolve, reject) => {
+		fs.writeFile(file, data, encodingType, (err) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve();
+			}
+		});
+	});
+}
+
+export function makeHttpsRequest(requestDefinition) {
+	return new Promise((resolve, reject) => {
+		https.get(requestDefinition, (response) => {
+			let data = '';
+			response.on('data', (chunk) => {
+			data += chunk;
+			});
+			response.on('end', () => {
+			resolve(data);
+			});
+		}).on('error', (err) => {
+			reject(err);
+		});
+	});
+}
+
+export function getAllFromTable(tableName) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const client = await pgPool.connect();
+            const result = await client.query(`SELECT * FROM ${tableName}`);
+
+            client.release();
+            resolve(result);
+        } catch (error) {
+            console.error(error);
+            reject(error);
+        }
+    });
 }
