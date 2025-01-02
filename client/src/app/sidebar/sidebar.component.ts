bb8a26b6db39933ecb38ac736bcf408c69f35cd0
marquisehaynes <haynes.marquise@gmail.com>
2025-01-01 21:56:42 -0600

    Frontend tweaks

--- client/src/app/sidebar/sidebar.component.ts
+++ client/src/app/sidebar/sidebar.component.ts
@@ -7,8 +7,11 @@ declare interface RouteInfo {
     icon: string;
     class: string;
 }
+
+/*
 export const ROUTES: RouteInfo[] = [
-    { path: '/dashboard', title: 'Dashboard',  icon: 'pe-7s-graph', class: '' },
+  { path: '/dashboard2', title: 'Dashboard',  icon: 'pe-7s-graph', class: '' },
+    { path: '/dashboard2', title: 'Dashboard2',  icon: 'pe-7s-graph', class: '' },
     { path: '/user', title: 'User Profile',  icon:'pe-7s-user', class: '' },
     { path: '/table', title: 'Table List',  icon:'pe-7s-note2', class: '' },
     { path: '/typography', title: 'Typography',  icon:'pe-7s-news-paper', class: '' },
@@ -16,6 +19,18 @@ export const ROUTES: RouteInfo[] = [
     { path: '/maps', title: 'Maps',  icon:'pe-7s-map-marker', class: '' },
     { path: '/notifications', title: 'Notifications',  icon:'pe-7s-bell', class: '' },
     { path: '/upgrade', title: 'Upgrade to PRO',  icon:'pe-7s-rocket', class: 'active-pro' },
+]; 
+*/
+
+export const ROUTES: RouteInfo[] = [
+    { path: '/dashboard', title: 'Dashboard',  icon: 'pe-7s-graph', class: '' },
+    { path: '/courses', title: 'Courses',  icon: 'pe-7s-graph', class: '' },
+    { path: '/students', title: 'Students',  icon: 'pe-7s-graph', class: '' },
+    { path: '/assignmentgroups', title: 'Assignment Groups',  icon:'pe-7s-user', class: '' },
+    { path: '/assignments', title: 'Assignments',  icon:'pe-7s-note2', class: '' },
+    { path: '/submissions', title: 'Assignment Submissions',  icon:'pe-7s-news-paper', class: '' },
+    { path: '/transcripts', title: 'Transcripts',  icon:'pe-7s-science', class: '' },
+    { path: '/timesheets', title: 'Timesheets',  icon:'pe-7s-map-marker', class: '' }
 ];
 
 @Component({
