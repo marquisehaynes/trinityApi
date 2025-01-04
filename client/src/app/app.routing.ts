61f78d1b81c926747377e9fa50eb99232bd3f9ce
marquisehaynes <haynes.marquise@gmail.com>
2025-01-04 02:53:45 -0600

    Update app.routing.ts

--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -10,14 +10,14 @@ const routes: Routes =[
     path: '',
     redirectTo: 'dashboard',
     pathMatch: 'full',
-  }, {
+  }, 
+  {
     path: '',
     component: AdminLayoutComponent,
     children: [
-        {
-      path: '',
-      loadChildren: () => import('./layouts/admin-layout/admin-layout.module').then(x => x.AdminLayoutModule)
-  }]},
+        {  path: '', loadChildren: () => import('./layouts/admin-layout/admin-layout.module').then(x => x.AdminLayoutModule) }
+    ]
+  },
   {
     path: '**',
     redirectTo: 'dashboard'
@@ -28,9 +28,7 @@ const routes: Routes =[
   imports: [
     CommonModule,
     BrowserModule,
-    RouterModule.forRoot(routes,{
-       useHash: true
-    })
+    RouterModule.forRoot( routes, { useHash: true } )
   ],
   exports: [
   ],
